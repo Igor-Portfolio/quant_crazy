7730abca5d5978f3ffff3d40529040c25e854f07
Igor-Portfolio <immachad752@gmail.com>
2026-07-23 23:38:11 -0500

    feat: hora de estudar e saber o que esta a fazer

--- test_all/graphical_visualization.py
+++ test_all/graphical_visualization.py
@@ -3,35 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import linear_algebra_logic as lg
 from src.domain.motor import *
+import pandas as pd
+
+numero_de_sep = 22
 
 df = yf.download(
     "JNJ",
-    start="2024-01-01",
+    start="2023-01-01",
     end="2025-01-01",
     auto_adjust=True,
     group_by="ticker"
 )
 
 
-print(df.head())
-
 x = np.arange(len(df.index))
 y = df[('JNJ', 'Close')].values
 
-list_x = maximuns_minumus(x, y, 30)
-print(list)
-df_index = df.set_index('Close')
-list_y = df_index.loc[list_x].values
+data = {'dias': x, 'prices': y}
+df_aux = pd.DataFrame(data)
+
+pass
+
+list_x = maximuns_minumus(df_aux['dias'].to_numpy(), df_aux['prices'].to_numpy(), numero_de_sep)
+print(list_x)
+
+list_y = df_aux.loc[list_x, 'prices']
 print(list_y)
 
 
-
-
-intervalos = divide_interval(x, 30)
+intervalos = divide_interval(x, numero_de_sep)
+cortes = [bloco[-1] for bloco in intervalos]
 print(intervalos)
 
 
-plt.plot(x, y)
-plt.show()
 plt.figure(figsize = (10,10))
+plt.plot(x, y)
+plt.scatter(list_x, list_y, color = 'red')
+plt.vlines(x=cortes, ymin=df_aux['prices'].min(), ymax=df_aux['prices'].max(), color = 'green')
+plt.show()
 plt.close()
